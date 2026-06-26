4d7ba91cd5407672a3f6e4300793c1eef9449015
techguru515 <bluesky.guru@outlook.com>
2026-06-26 18:14:17 +0200

    test: add failing tests for accept and arrive transitions

--- src/test/kotlin/com/cloudpay/taxi/RideServiceTest.kt
+++ src/test/kotlin/com/cloudpay/taxi/RideServiceTest.kt
@@ -19,4 +19,42 @@ class RideServiceTest {
         )
         assertIs<RideEvent.RideCreated>(service.getEvents(rideId).single())
     }
+
+    @Test
+    fun `accepting a pending ride stores a RideAccepted event and changes status to accepted`() {
+        val service = RideService()
+        val rideId = RideId("ride-1")
+
+        service.createRide(rideId)
+        service.acceptRide(rideId)
+
+        assertEquals(RideStatus.ACCEPTED, service.getStatus(rideId))
+        assertEquals(
+            listOf(
+                RideEvent.RideCreated(rideId),
+                RideEvent.RideAccepted(rideId),
+            ),
+            service.getEvents(rideId),
+        )
+    }
+
+    @Test
+    fun `marking driver arrived stores a DriverArrived event and changes status to waiting`() {
+        val service = RideService()
+        val rideId = RideId("ride-1")
+
+        service.createRide(rideId)
+        service.acceptRide(rideId)
+        service.markDriverArrived(rideId)
+
+        assertEquals(RideStatus.WAITING, service.getStatus(rideId))
+        assertEquals(
+            listOf(
+                RideEvent.RideCreated(rideId),
+                RideEvent.RideAccepted(rideId),
+                RideEvent.DriverArrived(rideId),
+            ),
+            service.getEvents(rideId),
+        )
+    }
 }
